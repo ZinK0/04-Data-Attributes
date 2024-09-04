8262a6633b09e71e789067cca1fb73a066d9c48b
Zin KO <zinko.it@outlook.com>
2024-09-04 10:10:35 +0630

    Successfully inplement the function plus toggler

--- Unsolved/assets/js/script.js
+++ Unsolved/assets/js/script.js
@@ -1,7 +1,17 @@
 var container = document.querySelector(".container");
 
-container.addEventListener("click", function(event) {
+container.addEventListener("click", function (event) {
   var element = event.target;
 
   // TODO: Complete function
+  // This if block will finish the task it check the data-state and add the class then the data
+  if (element.dataset.state === "hidden") {
+    element.dataset.state = "visible";
+    element.textContent = element.dataset.number;
+
+    // bonus for toggle the box with invert the value.
+  } else if (element.dataset.state === "visible") {
+    element.dataset.state = "hidden";
+    element.textContent = "";
+  }
 });
